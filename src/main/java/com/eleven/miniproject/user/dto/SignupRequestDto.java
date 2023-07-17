dd5d17cf6f689c879d27021cf1a87f1b87c3ebdd
soon91 <vbc1124@gmail.com>
2023-07-17 09:19:52 +0900

    회원가입, 로그인 구현

---
+++ src/main/java/com/eleven/miniproject/user/dto/SignupRequestDto.java
@@ -0,0 +1,16 @@
+package com.eleven.miniproject.user.dto;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignupRequestDto {
+    @Pattern(regexp = "^[a-z0-9]{4,10}$")
+    private String username;
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
+    private String password;
+}
