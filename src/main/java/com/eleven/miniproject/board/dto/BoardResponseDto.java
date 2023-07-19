1d2018a613745dae9406cd5491f390e6d498467e
hongsh429 <shh0429@kakao.com>
2023-07-19 14:35:23 +0900

    LocalDateTime

--- src/main/java/com/eleven/miniproject/board/dto/BoardResponseDto.java
+++ src/main/java/com/eleven/miniproject/board/dto/BoardResponseDto.java
@@ -15,10 +15,18 @@ public class BoardResponseDto {
     private String title;
     private String content;
     private String username;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            timezone = "Asia/Seoul")
     private LocalDateTime modifiedAt;
+
     private UploadImage uploadImage;
 
     public BoardResponseDto(Board savedBoard) {
